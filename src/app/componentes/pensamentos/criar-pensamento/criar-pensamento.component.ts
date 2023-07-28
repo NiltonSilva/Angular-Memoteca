a715e12d5ba4989f6b028ab3521aa324928c8c65
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-07-27 21:17:15 -0300

    feat: created the Create Thought component and configured the routes.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.scss']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.pensamento)
+  }
+
+  criarPensamento() {
+    alert("novo pensamento criado");
+  }
+
+  cancelarPensamento() {
+
+  }
 }
